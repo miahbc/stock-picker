0837a6c39617dabd0137753957a6771a3ceaf676
Miah Clay <miahb88@gmail.com>
2022-06-06 20:55:50 -0400

    working

--- js/stockPicker.js
+++ js/stockPicker.js
@@ -1,3 +1,11 @@
-exports.picker = function(prices) {
-
+function stock(prices) {
+    let prices2 = []
+    for (let i=0; i<prices.length; i++) {
+        prices2.push([prices[i],i])
+    }
+    console.log(prices2)
+    prices3 = prices2.sort((a,b) => a > b)
+    console.log(prices3)
 }
+
+console.log(stock([17,3,6,9,15,8,6,1,10]))
